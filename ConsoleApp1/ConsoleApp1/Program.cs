d15b89ac0772b2099b7e1f5924ee1c24c38af724
ringo-apo <ringo1625@gmail.com>
2018-04-06 18:52:45 +0900

    201804061859

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using Library1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string astr;
+            string bstr;
+            int a;
+            int b;
+            int c;
+            int d;
+
+            Console.WriteLine("数字を2つ入力してください");
+
+            astr = Console.ReadLine();
+            bstr = Console.ReadLine();
+
+            a = int.Parse(astr);
+            b = int.Parse(bstr);
+
+            c = Enzan.Tass(a, b);
+            d = Enzan.Hiku(a, b);
+
+            Console.WriteLine("{0}+{1}={2}", a, b, c);
+            Console.WriteLine("{0}-{1}={2}", a, b, d);
+
+        }
+    }
+}
---
+++ Library1/Library1/Library1.fs
@@ -0,0 +1,5 @@
+﻿namespace Library1
+
+module Enzan = 
+    let Tass a b = a + b
+    let Hiku a b = a - b
